31203c328ab533ed9b38aaf99b2430eaf60dc8cd
Dilan <dilandarren1@gmail.com>
2023-07-16 19:24:56 +0200

    first commit

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,9 @@
+// //Pregunta al usuario su nombre
+// const nombre = prompt('¿Cual es tu nombre ?');
+// //toma en nombre y lo muestra en pantalla
+// document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo JavaScrip Moderno`;
+
+// //Asingna el valor hacia la variable producto
+// const producto = 'Monitor 24 pulgadas'
+console.log('hola')
+console.log('mundo')
