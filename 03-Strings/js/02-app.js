490cb93c421be0807530b6967ad0633e3dd4f80d
Dilan <dilandarren1@gmail.com>
2023-07-16 20:36:23 +0200

    strings 02

---
+++ 03-Strings/js/02-app.js
@@ -0,0 +1,14 @@
+const producto = 'Monitor 20 Pulgadas'
+
+console.log(producto);
+
+//conocer la cantidad de letras de la cadena de texto
+console.log(producto.length);
+
+
+
+// console.log(producto.indexOf('tablet'));
+
+
+console.log(producto.includes('tablet'));
+console.log(producto.includes('Monitor'));
