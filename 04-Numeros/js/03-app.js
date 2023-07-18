2016affbbbdb89f1a48b8dca860e5a185a1d5748
Dilan <dilandarren1@gmail.com>
2023-07-18 21:17:14 +0200

    numeros

---
+++ 04-Numeros/js/03-app.js
@@ -0,0 +1,47 @@
+
+let resultado;
+
+
+
+//PI
+resultado = Math.PI;
+
+//Redondear
+resultado = Math.round(2.8);
+resultado = Math.round(2.7);
+resultado = Math.round(2.6);
+resultado = Math.round(2.5);
+resultado = Math.round(2.4);
+
+//Hacía arriba
+resultado = Math.ceil(2.1);
+
+//Abajo
+resultado = Math.floor(2.9);
+
+//Raiz Cuadrada
+resultado = Math.sqrt(43);
+
+//absoluto
+
+resultado = Math.sqrt(-500);
+
+//Potencia
+resultado = Math.pow(2, 4);
+
+
+//Minimo
+resultado = Math.min(3, 5, 1, 13, -12);
+
+//maximo
+resultado = Math.max(3, 5, 1, 13, -12);
+
+//Aleatorio
+resultado = Math.random() *20;
+
+resultado = Math.floor(Math.random() * 30);
+
+
+
+
+console.log(resultado);
