578477d7ab7622e674ea633f11d93c0f888c1d44
Dilan <dilandarren1@gmail.com>
2023-07-22 21:00:10 +0200

    add Arrays

---
+++ 08-Arrays/js/05-app.js
@@ -0,0 +1,37 @@
+// const meses = ['Enero', 'Febrero', 'Marzo'];
+
+// // meses[3] = 'Abril';
+
+// //Agregar al final del arreglo
+// meses.push('Abril');
+// meses.push('Mayo');
+
+// console.table(meses);
+
+
+const carrito = [];
+
+//definir un producto
+const producto = {
+    nombre: "Monitor 32 pulgadas",
+    precio: 400
+}
+
+const producto2 = {
+    nombre: "Movil",
+    precio: 800
+}
+
+//push agrega al final de un areglo
+
+carrito.push(producto);
+carrito.push(producto2);
+
+const producto3 = {
+    nombre: "Teclado",
+    precio: 50
+}
+
+carrito.unshift(producto3);
+
+console.table(carrito)
