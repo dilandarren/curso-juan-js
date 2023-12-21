b31d2ccdcf9d078a2e9e11d17ae86426dcab9ce4
Dilan <dilandarren1@gmail.com>
2023-12-21 20:57:25 +0100

    add 16

---
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -0,0 +1,77 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const email = {
+        email: '',
+        asunto: '',
+        mensaje: '',
+    }
+
+    //Seleccionar los elementos de la interfaz
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+
+    //Asignar eventos
+    inputEmail.addEventListener('blur', validar);
+    inputAsunto.addEventListener('blur', validar );
+    inputMensaje.addEventListener('blur', validar);
+    const formulario = document.querySelector('#formulario');
+
+    function validar(e) {
+        if(e.target.value.trim() === '' ) {
+            mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
+            return;
+        } 
+
+        if(e.target === 'email' && !validarEmail(e.target.value)) {
+            mostrarAlerta('el email no es valido', e.target.parentElement);
+            return;
+        };
+
+        limpiarAlerta(e.target.parentElement);
+
+        //Asignar valores
+        email[e.target.name] = e.target.value.trim(). toLowerCase();
+
+        //Comprobar el objeto de email
+        comprobarEmail();
+    }
+
+    
+
+    function mostrarAlerta(mensaje, referencia) {
+        limpiarAlerta(referencia);
+
+
+        // Generar alerta en HTML
+        const error = document.createElement('P');
+        error.textContent = mensaje;
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
+        
+
+
+
+        // Inyectar el error al formulario
+        referencia.appendChild(error)
+    }
+
+    function limpiarAlerta(referencia) {
+        //Comprueba si ya existe una alerta
+        const alerta = referencia.querySelector('.bg-red-600')
+        if(alerta) {
+            alerta.remove();
+        }
+    }
+
+    function validarEmail(email) {
+        const regex =  /^\w+([.-_+]?\w+)@\w+([.-]?\w+)(\.\w{2,10})+$/;
+
+        const resultado= regex.test(email);
+
+        console.log(resultado);
+    }
+
+    function comprobarEmail() {
+        console.log(Object.values(email).includes(''));
+    }
+});
