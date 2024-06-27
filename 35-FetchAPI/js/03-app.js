ad1d4bc145635c3d8c4b169b9571d2324f10a729
Dilan <dilandarren1@gmail.com>
2024-06-27 23:11:24 +0200

    add 35

---
+++ 35-FetchAPI/js/03-app.js
@@ -0,0 +1,35 @@
+const cargarJSONArrayBtn = document.querySelector('#cargarJSONArray');
+cargarJSONArrayBtn.addEventListener('click', obtenDatos);
+
+
+function obtenDatos() {
+    const url = 'data/empleados.json';
+
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => mostrarHTML(resultado));
+
+}
+
+
+function mostrarHTML(empleados) {
+    const contenido = document.querySelector('.contenido');
+
+    let html = '';
+
+
+    empleados.forEach(empleado => {
+        const { id, nombre, empresa, trabajo} = empleado;
+
+        
+        html += `
+            <p>Empleado : ${nombre}</p>
+            <p>ID : ${id}</p>
+            <p>Empresa : ${empresa}</p>
+            <p>trabajo : ${trabajo}</p>
+        `;
+    });
+
+    contenido.innerHTML = html;
+}
