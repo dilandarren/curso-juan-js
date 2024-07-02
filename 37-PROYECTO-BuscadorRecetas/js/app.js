09bdc065e237d9b797928b69f3c293ed4e4b8e3a
Dilan <dilandarren1@gmail.com>
2024-07-02 18:56:00 +0200

    finish project 37

--- 37-PROYECTO-BuscadorRecetas/js/app.js
+++ 37-PROYECTO-BuscadorRecetas/js/app.js
@@ -150,16 +150,26 @@ function iniciarApp() {
         modalBody.appendChild(listGroup);
 
         const modalFooter = document.querySelector('.modal-footer');
+        limpiarHTML(modalFooter);
 
         //Botton
         const btnFavorito = document.createElement('BUTTON');
         btnFavorito.classList.add('btn', 'btn-danger', 'col');
         btnFavorito.textContent = 'Guardar Favorito';
 
+        //ALMACENAR EN LOCALSTORAG
+        btnFavorito.onclick = function() {
+
+            //mostrar el modal
+            modal.hide();
+        }
 
         const btnCerrarModal = document.createElement('BUTTON');
-        btnCerrarModal.classList.add('btn', 'btn-secundary', 'col');
+        btnCerrarModal.classList.add('btn', 'btn-secondary', 'col');
         btnCerrarModal.textContent = 'Cerrar';
+        btnCerrarModal.onclick = function() {
+            modal.hide();
+        }
 
 
         modalFooter.appendChild(btnFavorito)
